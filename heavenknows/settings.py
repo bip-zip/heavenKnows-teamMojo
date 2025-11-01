3b9c053abda4793f0e74bbe4e9124e52172538d1
bip-zip <bipindhakal44@gmail.com>
2025-11-01 13:15:17 +0545

    fix req

--- heavenknows/settings.py
+++ heavenknows/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions', #for generating erds,
+    # 'django_extensions', #for generating erds,
     'accounts',
     'destinations',
     'packages',
